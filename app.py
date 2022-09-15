7522be371c602b111414ffca445b8b6a857f7785
Sneha Chendke <106645578+snehachendke98@users.noreply.github.com>
2022-09-15 13:27:37 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+import math
+
+app = Flask(__name__)
+model = pickle.load(open('build.pkl', 'rb'))  #imported model of ML
+
+@app.route('/')   #route folder
+def home():
+    return render_template('index.html')  # load form html
+
+@app.route('/predict',methods=['POST'])  # function need to run -- same should to be present in HTML 
+def predict(): 
+	int_features = [int(x) for x in request.form.values()]
+	final_features = [np.array(int_features)]
+	prediction = model.predict(final_features)
+	if prediction == 0:
+		return render_template('index.html',prediction_text="Customer is Good".format(prediction), # reload
+                               )
+	else:
+		return render_template('index.html',
+                               prediction_text="Customer is Bad".format(prediction), # reload
+                              )
+	
+if __name__ == "__main__":
+    app.run(debug=True)
